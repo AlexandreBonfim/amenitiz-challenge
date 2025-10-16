f58367410a3a022744a0b50ac322034a9e8de36a
Alexandre Santos <ale@MacBook-Pro-de-Alexandre.local>
2025-10-16 14:10:30 +0200

    feat: implemented bulk price rule

---
+++ backend/app/domain/cash_register/rules/bulk_price_rule.rb
@@ -0,0 +1,22 @@
+module CashRegister
+  module Rules
+    # If you buy threshold-or-more of a product, every unit of that product is priced at discounted_price
+    class BulkPriceRule < Rule
+      def initialize(product_code:, threshold:, discounted_price:)
+        @product_code      = product_code
+        @threshold         = threshold
+        @discounted_price  = BigDecimal(discounted_price.to_s)
+      end
+
+      def apply(items)
+        matching = items.select { |p| p.code == @product_code }
+
+        return 0.to_d if matching.count < @threshold
+        
+        unit_price = matching.first.price
+        discount_per_unit = (unit_price - @discounted_price)
+        (discount_per_unit * matching.count).round(2)
+      end
+    end
+  end
+end
--- backend/spec/domain/cash_register/checkout_spec.rb
+++ backend/spec/domain/cash_register/checkout_spec.rb
@@ -15,8 +15,18 @@ RSpec.describe CashRegister::Services::Checkout do
 
   subject(:subject) { described_class.new(pricing_rules: rules, catalog: catalog) }
 
-  it 'returns only 3.11 as BOGO rule' do
+  it 'returns 3.11 as BOGO rule' do
     subject.scan('GR1').scan('GR1')
+
     expect(subject.total).to eq(3.11.to_d)
+  end
+
+  it 'returns 16.61 as 3 strawberries have discount' do
+    catalog['SR1'] = CashRegister::Entities::Product.new(code: 'SR1', name: 'Strawberries', price: 5.00)
+    rules << CashRegister::Rules::BulkPriceRule.new(product_code: 'SR1', threshold: 3, discounted_price: 4.50)
+
+    %w[SR1 SR1 GR1 SR1].each { |c| subject.scan(c) }
+
+    expect(subject.total).to eq(16.61.to_d)
   end
 end
