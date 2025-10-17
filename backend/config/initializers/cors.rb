206df0f8b8af0aa3bbb03e1b6df52529b113da9d
Alexandre Santos <ale@MacBook-Pro-de-Alexandre.local>
2025-10-17 11:58:07 +0200

    chore: removed cors error

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -5,12 +5,14 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+frontend_origin = ENV.fetch("FRONTEND_ORIGIN", "*")
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins frontend_origin
+
+    resource "*",
+             headers: :any,
+             methods: %i[get post put patch delete options head]
+  end
+end
